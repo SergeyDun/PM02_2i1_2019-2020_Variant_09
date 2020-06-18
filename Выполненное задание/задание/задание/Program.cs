2499a09c8facb9f5483e0f66e99ba7a5b3264830
Sergey <56402515+SergeyDun@users.noreply.github.com>
2020-06-18 09:59:52 +0300

    МДК_02.02_пункт_2

---
+++ Выполненное задание/задание/задание/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите 1-е основание трапеции:");
+        double trapezoidBase1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите 2-е основание трапеции:");
+        double trapezoidBase2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите высоту трапеции:");
+        double trapezoidHeight = Convert.ToDouble(Console.ReadLine());
+
+        double areaOfTrapezoid = (trapezoidBase1 + trapezoidBase2) * trapezoidHeight / 2;
+        Console.WriteLine("Площадь трапеции равна: " + areaOfTrapezoid);
+        Console.Read();
+
+    }
+}
